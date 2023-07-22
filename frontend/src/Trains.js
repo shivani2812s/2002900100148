7c355933e0c911b857111ccf902eed91c239455a
Shivani Singh <shivanisingh36813@gmail.com>
2023-07-22 12:50:59 +0530

    routes updated

--- frontend/src/Trains.js
+++ frontend/src/Trains.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './train.css'
 import { useEffect,useState } from 'react';
 const Trains = () => {
     const [train, setTrain] = useState([]);
@@ -7,7 +8,7 @@ const Trains = () => {
           try {
             const response = await fetch('http://127.0.0.1:8000/api/train');
             const data = await response.json();
-            setTrain(data);
+            setTrain(data); //setting the train state with data being fetched
           } catch (error) {
             console.error('Error fetching train data:'+ error);
           }
@@ -15,26 +16,42 @@ const Trains = () => {
         fetchtrains();
         },  []);
   return (
-    <>
-     <table>
+   
+     <div className="TrainSchedule">
+      <h1>Train Schedule</h1>
+      <div className="table-wrapper">
+        <table>
           <thead>
             <tr>
+              <th>Train No</th>
               <th>Train Name</th>
               <th>Seats Available</th>
-              <th>Ticket price</th>
+              <th>Ticket Price</th>
+              <th>Departure time</th>
+              <th>Arrival time</th>
+              <th>Duration</th>
+              <th>Arrival station</th>
+              <th>Departure station</th>
             </tr>
           </thead>
           <tbody>
-          {train.map((data, i) => (
-              <tr key={i}>
-                <td>{data.train_name}</td>
-                <td>{data.seats_available}</td>
-                <td>{data.ticket_price}</td>
+            {train.map((trainItem) => (
+              <tr key={trainItem.train_id}>
+                <td>{trainItem.train_id}</td>
+                <td>{trainItem.train_name}</td>
+                <td>{trainItem.seats_available}</td>
+                <td>{trainItem.ticket_price}</td>
+                <td>{trainItem.departure_time}</td>
+                <td>{trainItem.arrival_time}</td>
+                <td>{trainItem.duration}</td>
+                <td>{trainItem.arrival_station}</td>
+                <td>{trainItem.departure_station}</td>
               </tr>
             ))}
           </tbody>
         </table>
-    </>
+      </div>
+    </div>
   )
 }
 
